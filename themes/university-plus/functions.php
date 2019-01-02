1d50bbce4a259d107dd449064c423b93153d5ed0
Tony Kwok <holypaladin07@gmail.com>
2019-01-02 01:06:17 -0800

    adding page based hero title subtitle and photo

--- themes/university-plus/functions.php
+++ themes/university-plus/functions.php
@@ -1,4 +1,33 @@
 <?php
+
+function pageBanner($args = NULL) {
+  // php logic will live here
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+  if (!$args['subtitle']) {
+    $args['subtitle'] = the_field('page_banner_subtitle');
+  }
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image')) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image"
+      style="background-image: url(<?php echo $args['photo']; ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php
+}
 function university_files() {
   wp_enqueue_script('main-university-javascript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts',
--- themes/university-plus/page.php
+++ themes/university-plus/page.php
@@ -2,17 +2,12 @@
 
   while(have_posts()) {
     the_post();
+    pageBanner(array(
+      'title' => 'Hello this is the title',
+      'subtitle' => 'This is the page subtitle',
+      'photo' => 'https://images-na.ssl-images-amazon.com/images/I/715jykrgmKL._SL1217_.jpg'
+    ));
   ?>
-
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
-      </div>
-    </div>
-  </div>
 
   <div class="container container--narrow page-section">
     <?php
