00ecb191d9b4b45756f9cc6622958c9e90c6625c
Tony Kwok <holypaladin07@gmail.com>
2019-01-02 13:19:35 -0800

    added reusable page banner code

--- themes/university-plus/archive.php
+++ themes/university-plus/archive.php
@@ -1,4 +1,9 @@
-<?php get_header(); ?>
+<?php get_header();
+pageBanner(array(
+  'title' => get_the_archive_title(),
+  'subtitle' => get_the_archive_description()
+));
+?>
 
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
