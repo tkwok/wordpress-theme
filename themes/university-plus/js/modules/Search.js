193ce64a85d2c37ad4e37132ac58a05723a13d96
Tony Kwok <holypaladin07@gmail.com>
2019-01-22 00:56:53 -0800

    added search all posts and pages

--- themes/university-plus/js/modules/Search.js
+++ themes/university-plus/js/modules/Search.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class Search {
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -24,7 +25,10 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
+
   }
 
   closeOverlay() {
@@ -52,13 +56,19 @@ class Search {
 
   getResults() {
     $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
+
+        var combinedResults = posts.concat(pages);
+
         this.resultsDiv.html(`
-          <h2 class="search-overlay__section-title">General Information</h2>
-            ${posts.length ? '<ul class="link-list min-list">': '<p>No Results</p>'}
-              ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</li>`).join('')}
-            ${posts.length ? '</ul>': ''}
-        `);
-        this.isSpinnerVisible = false;
+            <h2 class="search-overlay__section-title">General Information</h2>
+              ${combinedResults.length ? '<ul class="link-list min-list">': '<p>No Results</p>'}
+                ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</li>`).join('')}
+              ${combinedResults.length ? '</ul>': ''}
+          `);
+          this.isSpinnerVisible = false;
+        });
     });
   }
 
@@ -70,6 +80,24 @@ class Search {
     if (e.keyCode === 27 && this.isOverlayOpen) {
       this.closeOverlay();
     }
+  }
+
+  addSearchHTML() {
+      $("body").append(`
+        <div class="search-overlay">
+          <div class="search-overlay__top">
+            <div class="container">
+              <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+              <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+              <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div class="container">
+            <div id="search-overlay__results">
+            </div>
+          </div>
+        </div>
+      `);
   }
 }
 
